b6a901088875b704c6eaaa2e6c37e6c044364898
Bert Witzel <bwitzel82@gmail.com>
2020-10-12 13:03:46 -0400

    Work on search filter

--- js/app.js
+++ js/app.js
@@ -1,2 +1,18 @@
-baguetteBox.run('.gallery')
-const search = new Filter('search', 'data-caption');
+baguetteBox.run('#gallery')
+
+function searchFilter() {
+    var input, filter, ul, li, a, i, txtValue;
+    input = document.getElementById("myInput");
+    filter = input.value.toUpperCase();
+    div = document.getElementById("gallery");
+    li = div.getElementsByClassName("photo");
+    for (i = 0; i < li.length; i++) {
+        a = li[i].getElementsByTagName("a")[0];
+        txtValue = a.textContent || a.innerText;
+        if (txtValue.toUpperCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+    }
+}
